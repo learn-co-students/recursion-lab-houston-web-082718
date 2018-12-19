d1bb8a8aa2c537fb415ef81bc91324efb226f87b
jstricklin89 <jstricklin89@gmail.com>
2018-12-19 13:11:14 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if (string === "") {
+    return "";
+  } else {
+    return reverseString(string.substring(1)) + string.charAt(0);
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length === 1) {
+    return true;
+  } else {
+    if (string.charAt(0) === string.charAt(string.length - 1)) {
+      return isPalindrome(string.substring(1, string.length - 1));
+    }
+    return false;
+  }
+}
+
+function addUpTo(array, idx) {
+  if (idx) {
+    return array[idx] + addUpTo(array, --idx);
+  } else {
+    return array[idx];
+  }
+}
+
+function maxOf(array) {
+  if (array.length === 1) {
+    return array[0];
+  } else {
+    return Math.max(array.pop(), maxOf(array));
+  }
+}
+
+function includesNumber(array, number) {
+  if (array.length === 0) {
+    return false;
+  } else if (array[0] === number) {
+    return true;
+  } else {
+    return includesNumber(array.slice(1), number);
+  }
+}
